f40d4d85811c082b6842e2d2dc038bf206424b44
Paul Mauviel <p.a.mauviel@gmail.com>
2020-07-26 22:06:23 -0400

    Update build-clients.sh

--- gddragonbones/bin-build/build-clients.sh
+++ gddragonbones/bin-build/build-clients.sh
@@ -8,7 +8,7 @@
 rm -rf ./build
 mkdir -p ./build
 
-docker build --build-arg UBUNTU_VERSION="16.04". -t godot-dragonbones:x11
+docker build --build-arg UBUNTU_VERSION="16.04" . -t godot-dragonbones:x11
 docker build . -t godot-dragonbones:other
 
 # Build clients
