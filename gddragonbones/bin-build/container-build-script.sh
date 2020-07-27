aa1ff2d28ced72c1cd0267ac0ed5a5751d5734d6
Paul Mauviel <p.a.mauviel@gmail.com>
2020-07-26 20:13:22 -0400

    Python3 stuff

--- gddragonbones/bin-build/container-build-script.sh
+++ gddragonbones/bin-build/container-build-script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # windows
-scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
-scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
+/usr/local/bin/scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
+/usr/local/bin/scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
 
 # x11
-scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
-scons -j8 platform=x11 target=release_debug bits=64 use_lto=yes
+/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
+/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=64 use_lto=yes
 
 #osx
-scons -j8 platform=osx target=release_debug use_lto=yes osxcross_sdk=darwin19
+/usr/local/bin/scons -j8 platform=osx target=release_debug use_lto=yes osxcross_sdk=darwin19
