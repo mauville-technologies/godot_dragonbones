6da812259b1def52e1a35e47fc32e14a2aa630aa
Paul Mauviel <p.a.mauviel@gmail.com>
2020-07-26 22:48:52 -0400

    Update Dockerfile, build-clients.sh, and 4 more files...

--- gddragonbones/bin-build/container-build-script.sh
+++ gddragonbones/bin-build/container-build-script.sh
@@ -2,7 +2,7 @@
 
 echo "BUILDING WITH UBUNTU VERSION ${1}"
 
-if [ "$1" = "16.04" ]
+if [ "$1" = "16.04"]
 then
     # x11
     /usr/local/bin/scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
@@ -11,8 +11,7 @@ else
     # windows
     /usr/local/bin/scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
     /usr/local/bin/scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
-
-
+    
     #osx
     /usr/local/bin/scons -j8 platform=osx target=release_debug use_lto=yes osxcross_sdk=darwin19
 fi
--- gddragonbones/bin-export-templates/container-build-script.sh
+++ gddragonbones/bin-export-templates/container-build-script.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# TODO: Change these to export templates commands.
+echo "BUILDING WITH UBUNTU VERSION ${1}"
 
-# windows
-/usr/local/bin/scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
-/usr/local/bin/scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
-
-# x11
-/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
-/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=64 use_lto=yes
-
-#osx
-/usr/local/bin/scons -j8 platform=osx target=release_debug use_lto=yes osxcross_sdk=darwin19
-
-# android
-
-# iphone
-
-# HTML5
-
+if [ "$1" = "16.04"]
+then
+    # x11
+    /usr/local/bin/scons -j8 platform=x11 tools=no target=release bits=32 use_lto=yes
+    /usr/local/bin/scons -j8 platform=x11 tools=no target=release_debug bits=32 use_lto=yes
+    /usr/local/bin/scons -j8 platform=x11 tools=no target=release bits=64 use_lto=yes
+    /usr/local/bin/scons -j8 platform=x11 tools=no target=release_debug bits=64 use_lto=yes
+else
+    # windows
+    /usr/local/bin/scons -j8 platform=windows tools=no target=release_debug bits=32 use_lto=yes
+    /usr/local/bin/scons -j8 platform=windows tools=no target=release bits=32 use_lto=yes
+    /usr/local/bin/scons -j8 platform=windows tools=no target=release_debug bits=64 use_lto=yes
+    /usr/local/bin/scons -j8 platform=windows tools=no target=release bits=64 use_lto=yes
+    
+    #osx
+    /usr/local/bin/scons -j8 platform=osx tools=no target=release use_lto=yes osxcross_sdk=darwin19
+    /usr/local/bin/scons -j8 platform=osx tools=no target=release_debug use_lto=yes osxcross_sdk=darwin19
+fi
 
 # Copy all builds to builds directory
 cp -avr ./bin/. /build
