af7f0794936b4aa6076ed93c2b6b0e70a4c4c9b6
Paul Mauviel <p.a.mauviel@gmail.com>
2020-07-28 16:01:13 -0400

    Update Dockerfile, build-clients.sh, and container-build-script.sh

--- gddragonbones/bin-build/build-clients.sh
+++ gddragonbones/bin-build/build-clients.sh
@@ -3,14 +3,7 @@
 # This is the top file called
 
 # Steps:
-
-# clean/create build dir
-rm -rf ./build
-mkdir -p ./build
-
-docker build --build-arg UBUNTU_VERSION="16.04" . -t godot-dragonbones:x11
-docker build . -t godot-dragonbones:other
+docker build . -t godot-dragonbones:x11
 
 # Build clients-v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross 
 docker run -v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross -it godot-dragonbones:x11
-docker run -v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross -it godot-dragonbones:other
--- gddragonbones/bin-build/container-build-script.sh
+++ gddragonbones/bin-build/container-build-script.sh
@@ -2,15 +2,15 @@
 
 
 
-if [ "$1" = "17.04" ]
-then
+# if [ "$1" = "17.04" ]
+# then
     echo "BUILDING WITH UBUNTU VERSION ${1}"
     cd $GODOT_SOURCE_LOCATION
     
     # x11
-    /usr/local/bin/scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
-    /usr/local/bin/scons -j8 platform=x11 target=release_debug bits=64 use_lto=yes
-else
+    #/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=32 use_lto=yes
+    #/usr/local/bin/scons -j8 platform=x11 target=release_debug bits=64 use_lto=yes
+# else
     echo "BUILDING WITH UBUNTU VERSION ${1}"
 
     cd $OSXCROSS_ROOT
@@ -18,12 +18,12 @@ else
 
     cd $GODOT_SOURCE_LOCATION
     # windows
-    /usr/local/bin/scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
-    /usr/local/bin/scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
+    #/usr/local/bin/scons -j8 platform=windows target=release_debug bits=32 use_lto=yes
+    #/usr/local/bin/scons -j8 platform=windows target=release_debug bits=64 use_lto=yes
 
     #osx
     /usr/local/bin/scons -j8 platform=osx target=release_debug use_lto=yes osxcross_sdk=darwin19
-fi
+# fi
 
 # Copy all builds to builds directory
 cp -av bin/. /build/engine
