c53bbaefba96a246e05c4c0ea5c99d1013c9e684
Paul Mauviel <p.a.mauviel@gmail.com>
2020-07-28 02:06:37 -0400

    I think this is getting closer

--- gddragonbones/bin-build/build-clients.sh
+++ gddragonbones/bin-build/build-clients.sh
@@ -8,9 +8,9 @@
 rm -rf ./build
 mkdir -p ./build
 
-docker build --build-arg UBUNTU_VERSION="16.04" . -t godot-dragonbones:x11
+#docker build --build-arg UBUNTU_VERSION="16.04" . -t godot-dragonbones:x11
 docker build . -t godot-dragonbones:other
 
 # Build clients-v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross 
-docker run -v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross -it godot-dragonbones:x11
+#docker run -v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross -it godot-dragonbones:x11
 docker run -v dragonbonesDir:/build -v /home/ozzadar/src/osxcross:/osxcross -it godot-dragonbones:other
