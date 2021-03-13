7a75e1a2c5613ceee6d045401a1e6bd7c661e81a
Davor <rmilitarev22@gmail.com>
2021-03-13 13:53:44 +0100

    Expose bone position parameters to Godot

--- src/GDBone2D.h
+++ src/GDBone2D.h
@@ -39,11 +39,19 @@ public:
 	String get_bone_name();
 	Vector2 get_bone_position();
 	void set_bone_position(Vector2 new_pos);
+	void set_bone_global_position(Vector2 new_pos);
 	Vector2 get_bone_scale();
 	void set_bone_scale(Vector2 scale);
+	void set_bone_global_scale(Vector2 scale);
 	float get_bone_rotation();
 	void set_bone_rotation(float rotation);
-
+	void set_bone_global_rotation(float rotation);
+	Vector2 get_bone_global_position();
+	Vector2 get_bone_global_scale();
+	float   get_bone_global_rotation();
+	Vector2 get_bone_offset_position();
+	Vector2 get_bone_animation_position();
+	Vector2 get_bone_origin_position();
 	bool has_data();
 };
 
